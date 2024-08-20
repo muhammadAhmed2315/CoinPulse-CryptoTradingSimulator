cab169162909a37e7aaa3932e4e7f93d1e918683
Muhammad Ahmed <muhammad.ahmed2315@gmail.com>
2024-08-20 21:27:50 +0100

    Created Discord oauth branch

--- app.py
+++ app.py
@@ -76,5 +76,4 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    # Run the app
     app.run()
--- login/app.py
+++ login/app.py
@@ -8,21 +8,90 @@ from login.forms import LoginForm, RegisterForm
 from validate_email_address import validate_email
 from constants import PASSWORD_ALLOWED_SPECIAL_CHARS
 from flask import render_template, redirect, request, url_for, Blueprint, session
+from flask import jsonify
 from flask_login import login_user, login_required, logout_user, current_user
 
 
 user_authentication = Blueprint("user_authentication", __name__)
 
+# #################### DISCORD OAUTH AUTHENTICATION ####################
+DISCORD_OAUTH2_CLIENT_ID = ""
+DISCORD_OAUTH2_CLIENT_SECRET = ""
+DISCORD_OAUTH2_REDIRECT_URI = "http://127.0.0.1:5000/callback_discord"
+
+DISCORD_API_BASE_URL = os.environ.get("API_BASE_URL", "https://discordapp.com/api")
+DISCORD_AUTHORIZATION_BASE_URL = DISCORD_API_BASE_URL + "/oauth2/authorize"
+DISCORD_TOKEN_URL = DISCORD_API_BASE_URL + "/oauth2/token"
+
+
+def token_updater(token):
+    session["oauth2_token"] = token
+
+
+def make_session(token=None, state=None, scope=None):
+    return OAuth2Session(
+        client_id=DISCORD_OAUTH2_CLIENT_ID,
+        token=token,
+        state=state,
+        scope=scope,
+        redirect_uri=DISCORD_OAUTH2_REDIRECT_URI,
+        auto_refresh_kwargs={
+            "client_id": DISCORD_OAUTH2_CLIENT_ID,
+            "client_secret": DISCORD_OAUTH2_CLIENT_SECRET,
+        },
+        auto_refresh_url=DISCORD_TOKEN_URL,
+        token_updater=token_updater,
+    )
+
+
+@user_authentication.route("/login_discord")
+def login_discord():
+    scope = request.args.get("scope", "identify email")
+    discord = make_session(scope=scope.split(" "))
+    authorization_url, state = discord.authorization_url(DISCORD_AUTHORIZATION_BASE_URL)
+    session["oauth2_state"] = state
+    return redirect(authorization_url)
+
+
+@user_authentication.route("/callback_discord")
+def callback_discord():
+    if request.values.get("error"):
+        return request.values["error"]
+    discord = make_session(state=session.get("oauth2_state"))
+    token = discord.fetch_token(
+        DISCORD_TOKEN_URL,
+        client_secret=DISCORD_OAUTH2_CLIENT_SECRET,
+        authorization_response=request.url,
+    )
+    session["oauth2_token"] = token
+
+    discord = make_session(token=session.get("oauth2_token"))
+    user_info = jsonify(discord.get(DISCORD_API_BASE_URL + "/users/@me").json())
+    user_email = user_info["user"]["email"]
+    user_id = user_info["user"]["id"]
+
+    # Check if user already exists in the database
+    user = User.query.filter_by(email=user_email).first()
+    if not user:
+        # Create the user, add to the database and then login
+        user = User(email=user_email, provider="google", provider_id=user_id)
+        db.session.add(user)
+        db.session.commit()
+
+    login_user(user)
+    return redirect(url_for("home.index"))
+
+
 # #################### GOOGLE OAUTH AUTHENTICATION ####################
 # OAuth configuration via environment variables
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"  # Only for development on localhost
-client_id = ""
-client_secret = ""
-authorization_base_url = "https://accounts.google.com/o/oauth2/auth"
-token_url = "https://accounts.google.com/o/oauth2/token"
-redirect_uri = "http://127.0.0.1:5000/google_callback"
-userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
-scope = ["profile", "email"]
+google_client_id = ""
+google_client_secret = ""
+google_authorization_base_url = "https://accounts.google.com/o/oauth2/auth"
+google_token_url = "https://accounts.google.com/o/oauth2/token"
+google_redirect_uri = "http://127.0.0.1:5000/callback_google"
+google_userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+google_scope = ["profile", "email"]
 
 
 @user_authentication.route("/login_google")
@@ -41,16 +110,18 @@ def login_google():
         Redirect: A redirection response object that directs the user to Google's
                   OAuth2 login page.
     """
-    google = OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scope)
+    google = OAuth2Session(
+        google_client_id, redirect_uri=google_redirect_uri, scope=google_scope
+    )
     authorization_url, state = google.authorization_url(
-        authorization_base_url, access_type="offline", prompt="select_account"
+        google_authorization_base_url, access_type="offline", prompt="select_account"
     )
     session["oauth_state"] = state
     return redirect(authorization_url)
 
 
-@user_authentication.route("/google_callback")
-def google_callback():
+@user_authentication.route("/callback_google")
+def callback_google():
     """
     Handles the callback from Google OAuth2 authentication.
 
@@ -70,15 +141,19 @@ def google_callback():
     """
     # Fetches and saves token in the session data
     google = OAuth2Session(
-        client_id, state=session.get("oauth_state"), redirect_uri=redirect_uri
+        google_client_id,
+        state=session.get("oauth_state"),
+        redirect_uri=google_redirect_uri,
     )
     token = google.fetch_token(
-        token_url, client_secret=client_secret, authorization_response=request.url
+        google_token_url,
+        client_secret=google_client_secret,
+        authorization_response=request.url,
     )
     session["google_token"] = token
 
     # Getting user email
-    userinfo_response = google.get(userinfo_url)
+    userinfo_response = google.get(google_userinfo_url)
     userinfo = userinfo_response.json()
     user_email = userinfo.get("email")
     user_id = userinfo.get("id")
@@ -105,7 +180,7 @@ def login():
     they were trying to access.
     """
     if current_user.is_authenticated:
-        return redirect(url_for("home.index"))  # TODO
+        return redirect(url_for("home.index"))
 
     form = LoginForm()
 

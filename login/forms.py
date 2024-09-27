572220193806bb2fa315092831fcc782d0595fc6
Muhammad Ahmed <muhammad.ahmed2315@gmail.com>
2024-09-27 21:43:49 +0100

    Finished styling register page

--- login/forms.py
+++ login/forms.py
@@ -37,24 +37,25 @@ class RegisterForm(FlaskForm):
     """
 
     email = StringField(
-        "Email",
-        render_kw={"placeholder": "john@example.com"},
+        render_kw={"placeholder": "Email", "class": "input input--email"},
         validators=[DataRequired()],
     )
     username = StringField(
-        "Username", render_kw={"placeholder": "john2024"}, validators=[DataRequired()]
+        render_kw={"placeholder": "Username", "class": "input input--username"},
+        validators=[DataRequired()],
     )
     password = PasswordField(
-        "Password",
-        render_kw={"placeholder": "******************"},
+        render_kw={"placeholder": "Password", "class": "input input--password"},
         validators=[DataRequired()],
     )
     pass_confirm = PasswordField(
-        "Confirm Password",
-        render_kw={"placeholder": "******************"},
+        render_kw={
+            "placeholder": "Confirm Password",
+            "class": "input input--pass-confirm",
+        },
         validators=[DataRequired()],
     )
-    submit = SubmitField("Sign Up")
+    submit = SubmitField("Create Your Account", render_kw={"class": "submit-btn"})
 
 
 class RequestPasswordResetForm(FlaskForm):

80574b2ca05069092cae823a1aad72fcba79a7b7
muhammadAhmed2315 <muhammad.ahmed2315@gmail.com>
2026-03-19 17:29:31 +0000

    Login.tsx: Add React component for login page

--- frontend/src/components/authentication/Login.tsx
+++ frontend/src/components/authentication/Login.tsx
@@ -3,7 +3,10 @@ import discordLogo from "@/assets/logos/discord.svg";
 import googleLogo from "@/assets/logos/google.svg";
 import axios from "axios";
 
-import { Button } from "@/components/ui/button";
+import {
+  RippleButton,
+  RippleButtonRipples,
+} from "@/components/animate-ui/components/buttons/ripple";
 import {
   Card,
   CardContent,
@@ -16,21 +19,44 @@ import { Field, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Label } from "../ui/label";
 import { useMutation } from "@tanstack/react-query";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
+import { AlertCircleIcon } from "lucide-react";
+import { validateEmail } from "@/utils";
+import { Spinner } from "../ui/spinner";
+
+async function loginFunction(data: { email: string; password: string }) {
+  const response = await fetch("http://127.0.0.1:5000/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+    credentials: "include",
+  });
+
+  if (!response.ok) throw await response.json();
+
+  return await response.json();
+}
 
 export default function Login() {
-  const [email, setEmail] = useState("");
+  const prefillEmail: string = useLocation().state?.prefillEmail ?? "";
+  const [email, setEmail] = useState(prefillEmail);
   const [password, setPassword] = useState("");
+  const [error, setError] = useState<[string, string]>(["", ""]);
   const navigate = useNavigate();
 
   const loginMutation = useMutation({
-    mutationFn: (data: { email: string; password: string }) =>
-      axios.post("http://127.0.0.1:5000/login", data, {
-        withCredentials: true,
-      }),
+    mutationFn: (data: { email: string; password: string }) => {
+      return loginFunction(data);
+    },
 
     onSuccess: () => {
       navigate("/dashboard");
+    },
+
+    onError: (error) => {
+      const err = error as unknown as { error: string; description: string };
+      setError([err.error ?? "Login failed", err.description ?? ""]);
     },
   });
 
@@ -46,10 +72,22 @@ export default function Login() {
   });
 
   async function handleLogin(): Promise<void> {
-    loginMutation.mutate({
-      email,
-      password,
-    });
+    if (!validateEmail(email)) {
+      setError([
+        "Invalid email address",
+        "Please enter a valid email address (e.g., john.doe@gmail.com)",
+      ]);
+    } else if (password.length < 8) {
+      setError([
+        "Invalid password",
+        "Password must be at least 8 characters long",
+      ]);
+    } else {
+      loginMutation.mutate({
+        email,
+        password,
+      });
+    }
   }
 
   async function handleTestAccountLogin(): Promise<void> {
@@ -57,11 +95,11 @@ export default function Login() {
   }
 
   function handleForgotPassword() {
-    navigate("/reset_password");
+    navigate("/request_password_reset");
   }
 
   function handleRegister() {
-    navigate("/register");
+    navigate("/create_account");
   }
 
   function handleEmailInput(e: ChangeEvent<HTMLInputElement>): void {
@@ -95,13 +133,12 @@ export default function Login() {
         <Field>
           <div className="flex items-center">
             <Label htmlFor="input-password">Password</Label>
-            <a
-              href="#"
-              className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+            <p
+              className="ml-auto inline-block text-sm underline-offset-4 hover:underline cursor-pointer"
               onClick={handleForgotPassword}
             >
               Forgot your password?
-            </a>
+            </p>
           </div>
           <Input
             id="input-password"
@@ -111,23 +148,35 @@ export default function Login() {
             onChange={handlePasswordInput}
           />
         </Field>
+        {error.at(0) !== "" && error.at(1) !== "" && (
+          <>
+            <br />
+            <Alert variant="destructive" className="max-w-md">
+              <AlertCircleIcon />
+              <AlertTitle>{error.at(0)}</AlertTitle>
+              <AlertDescription>{error.at(1)}</AlertDescription>
+            </Alert>
+          </>
+        )}
       </CardContent>
       <CardFooter className="flex flex-col gap-2.5">
-        <Button
+        <RippleButton
           className="w-full cursor-pointer"
           variant="outline"
           onClick={handleLogin}
         >
-          Login
-        </Button>
+          {loginMutation.isPending ? <Spinner /> : <>Login</>}
+          <RippleButtonRipples />
+        </RippleButton>
 
-        <Button
+        <RippleButton
           className="w-full cursor-pointer"
           variant="outline"
           onClick={handleTestAccountLogin}
         >
           Login to test account
-        </Button>
+          <RippleButtonRipples />
+        </RippleButton>
 
         <div className="flex w-full items-center">
           <div className="flex-1 border-t border-gray-400" />
@@ -138,33 +187,35 @@ export default function Login() {
         </div>
 
         <div className="flex gap-2 justify-center">
-          <Button className="cursor-pointer" variant="outline">
+          <RippleButton className="cursor-pointer" variant="outline">
             <img
               className="h-6 w-6 cursor-pointer"
               src={discordLogo}
               alt="Discord logo"
             />
             Discord
-          </Button>
+            <RippleButtonRipples />
+          </RippleButton>
 
-          <Button className="cursor-pointer" variant="outline">
+          <RippleButton className="cursor-pointer" variant="outline">
             <img
               className="h-5.5 w-5.5 cursor-pointer"
               src={googleLogo}
               alt="Google logo"
             />
             Google
-          </Button>
+            <RippleButtonRipples />
+          </RippleButton>
         </div>
 
-        <div className="flex items-center">
-          <a
-            href="#"
-            className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+        <div className="flex items-center text-sm">
+          <span>Don't have an account?&nbsp;</span>
+          <p
+            className="ml-auto inline-block underline-offset-4 hover:underline cursor-pointer"
             onClick={handleRegister}
           >
-            Don't have an account? Sign up here
-          </a>
+            Sign up here
+          </p>
         </div>
       </CardFooter>
     </Card>
